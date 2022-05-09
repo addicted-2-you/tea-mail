4f33aab6061491595a0a84176a095c955e130436
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2022-05-09 15:23:38 +0500

    add mocks

--- db-scripts/messaging/create-messaging.sql
+++ db-scripts/messaging/create-messaging.sql
@@ -19,25 +19,30 @@ create table tea_mail.chats (
   deletedat datetime default null
 );
 
+insert into tea_mail.chats (id, title) values (0, "тест");
+
 drop table if exists tea_mail.users_chats;
 create table tea_mail.users_chats (
   chatid int not null,
   userid int not null
 );
 
+insert into tea_mail.users_chats (chatid, userid) values (0, 0); -- 0 is admin
+insert into tea_mail.users_chats (chatid, userid) values (0, -1); -- -1 is test user
+
 drop table if exists tea_mail.messages;
 create table tea_mail.messages (
   id int primary key auto_increment,
-  text varchar(255) default null,
+  messagetext varchar(255) default null,
   url  varchar(255) default null,
   senderid int not null,
   chatid int not null,
   
   createdat datetime default now(),
   updatedat datetime default null on update now(),
-  deletedat datetime default null
+  deletedat datetime default null,
   
-  constraint message_complete check (text is not null or url is not null)
+  constraint message_complete check (messagetext is not null or url is not null)
 );
 
 
