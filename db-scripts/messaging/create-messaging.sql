2dc87d31c979ff74b3de48fe7fff88b3e1394e4d
Aliaksandr Kavalenka <40535982+slnchn@users.noreply.github.com>
2022-05-09 15:24:09 +0500

    add basic messaging table structure (#2)
    * add basic messaging table structure
    
    * add mocks

---
+++ db-scripts/messaging/create-messaging.sql
@@ -0,0 +1,48 @@
+drop table if exists tea_mail.users;
+create table tea_mail.users (
+  id int primary key auto_increment,
+  username varchar(255) not null,
+  password varchar(255) not null,
+
+  createdat datetime default now(),
+  updatedat datetime default null on update now(),
+  deletedat datetime default null
+);
+
+drop table if exists tea_mail.chats;
+create table tea_mail.chats (
+  id int primary key auto_increment,
+  title varchar(255) default null,
+
+  createdat datetime default now(),
+  updatedat datetime default null on update now(),
+  deletedat datetime default null
+);
+
+insert into tea_mail.chats (id, title) values (0, "тест");
+
+drop table if exists tea_mail.users_chats;
+create table tea_mail.users_chats (
+  chatid int not null,
+  userid int not null
+);
+
+insert into tea_mail.users_chats (chatid, userid) values (0, 0); -- 0 is admin
+insert into tea_mail.users_chats (chatid, userid) values (0, -1); -- -1 is test user
+
+drop table if exists tea_mail.messages;
+create table tea_mail.messages (
+  id int primary key auto_increment,
+  messagetext varchar(255) default null,
+  url  varchar(255) default null,
+  senderid int not null,
+  chatid int not null,
+  
+  createdat datetime default now(),
+  updatedat datetime default null on update now(),
+  deletedat datetime default null,
+  
+  constraint message_complete check (messagetext is not null or url is not null)
+);
+
+
