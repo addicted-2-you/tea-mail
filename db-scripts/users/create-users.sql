2b4cbd825ce4f46710233ec9a62bef4537140dc5
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2022-05-15 14:51:18 +0500

    add users tables

---
+++ db-scripts/users/create-users.sql
@@ -0,0 +1,31 @@
+-- drop table if exists tea_mail.users;
+create table if not exists tea_mail.users (
+  id int primary key auto_increment,
+  username varchar(255) not null,
+  password varchar(255) not null,
+
+  createdat datetime default now(),
+  updatedat datetime default null on update now(),
+  deletedat datetime default null
+);
+
+drop table if exists tea_mail.roles;
+create table tea_mail.roles (
+  id int primary key auto_increment,
+  title varchar(255) not null,
+  
+  createdat datetime default now(),
+  updatedat datetime default null on update now(),
+  deletedat datetime default null
+);
+
+insert into tea_mail.roles (id, title) values (1, "Admin");
+insert into tea_mail.roles (id, title) values (2, "User");
+
+drop table if exists tea_mail.users_roles;
+create table tea_mail.users_roles (
+  userid int not null,
+  roleid int not null,
+  
+  unique key userrolepair (userid, roleid)
+);
