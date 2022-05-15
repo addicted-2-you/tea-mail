f9e80b43337958820c8e71a46e951ab5a04c6ae8
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2022-05-15 16:46:46 +0500

    set context if the user is authorized

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -4,6 +4,7 @@ import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { createConnection } from 'typeorm';
+import jwt from 'jsonwebtoken';
 
 // graphql
 import { graphqlHTTP } from 'express-graphql';
@@ -25,8 +26,10 @@ import { Tea } from '~/graphql/schema/entities/Tea';
 import { Portion } from '~/graphql/schema/entities/Portion';
 import { Chat } from '~/graphql/schema/entities/Chat';
 import { Message } from '~/graphql/schema/entities/Message';
+import { User } from '~/graphql/schema/entities/User';
 
 // resolvers
+import { AuthResolver } from '~/graphql/schema/resolvers/AuthResolver';
 import { TeaResolver } from '~/graphql/schema/resolvers/TeaResolver';
 import { PortionResolver } from '~/graphql/schema/resolvers/PortionResolver';
 import { ChatResolver } from '~/graphql/schema/resolvers/ChatResolver';
@@ -63,18 +66,33 @@ async function runApp() {
     username: 'root',
     password: 'root',
     database: 'tea_mail',
-    entities: [Tea, Portion, Chat, Message],
+    entities: [Tea, Portion, Chat, Message, User],
     // logging: true,
     synchronize: false,
   });
 
   app.use(
     '/graphql',
-    graphqlHTTP({
-      graphiql: true,
-      schema: await buildSchema({
-        resolvers: [TeaResolver, PortionResolver, ChatResolver, MessageResolver],
-      }),
+    graphqlHTTP(async (req, res) => {
+      const token = req.headers.authorization;
+      const context = {};
+      if (!token) {
+        context.user = null;
+      } else {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log(decoded);
+        context.user = decoded;
+      }
+
+      return {
+        graphiql: true,
+
+        schema: await buildSchema({
+          resolvers: [AuthResolver, TeaResolver, PortionResolver, ChatResolver, MessageResolver],
+        }),
+
+        context: { ...req, ...context },
+      };
     }),
   );
 
