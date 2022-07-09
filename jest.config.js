406821ef97bf88cd471b1248f969eb67994b2111
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2022-07-10 00:04:32 +0500

    add storybook

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   moduleNameMapper: {
-    '^~/(.*).svg$': '<rootDir>/tests/mocks/SvgMock.js', // mock importing svg files
+    '^~/(.*).svg$': '<rootDir>/mocks/SvgMock.js', // mock importing svg files
     '^~/(.*)$': '<rootDir>/src/$1',
   },
 
---
+++ mocks/tea-mocks.js
@@ -0,0 +1,62 @@
+export const MILK_OOLONG_TEA = {
+  id: 1,
+  title: 'Молочный Улун',
+  price: 10,
+  portions: [
+    { id: 1, title: '5 г.' },
+    { id: 2, title: '10 г.' },
+    { id: 3, title: '15 г.' },
+    { id: 4, title: '25 г.' },
+    { id: 5, title: '50 г.' },
+  ],
+};
+
+export const GRAPE_OOLONG_TEA = {
+  id: 2,
+  title: 'Сливчоный Виноградный Улун',
+  price: 25,
+  portions: [
+    { id: 4, title: '25 г.' },
+    { id: 5, title: '50 г.' },
+  ],
+};
+
+export const OLD_OOLONG_TEA = {
+  id: 3,
+  title: 'Древний Улун',
+  price: 100,
+  portions: [
+    { id: 6, title: '100 г.' },
+    { id: 7, title: '200 г.' },
+    { id: 8, title: '1 шт.' },
+    { id: 9, title: '2 шт.' },
+  ],
+};
+
+export const HARE_OOLONG_TEA = {
+  id: 4,
+  title: 'Заяц Гун Тин',
+  price: 10,
+  portions: [
+    { id: 10, title: '3 шт.' },
+    { id: 11, title: '4 шт.' },
+  ],
+};
+
+export const GOO_TIN_BRICK_TEA = {
+  id: 5,
+  title: 'Гу Тин в кирпиче (из почечного сырья)',
+  price: 10,
+  portions: [
+    { id: 12, title: '5 шт.' },
+    { id: 13, title: '6 шт.' },
+  ],
+};
+
+export const teaArray = [
+  MILK_OOLONG_TEA,
+  GRAPE_OOLONG_TEA,
+  OLD_OOLONG_TEA,
+  HARE_OOLONG_TEA,
+  GOO_TIN_BRICK_TEA,
+];
